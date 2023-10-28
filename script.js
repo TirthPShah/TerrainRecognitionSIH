2bc01c9ecb8bbcb4791c51f681e4f71059df6066
Tirth Shah <cptirthp.shah@gmail.com>
2023-10-28 19:15:28 +0530

    CSS Applied to Whole Page and Awesome Footer Constructed

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
     const formData = new FormData(this);
+
+    const selectedImage = document.getElementById('selectedImage');
+
+    // Handle the selected file and update the img src
+    const fileInput = formData.get('file');
+    const fileURL = URL.createObjectURL(fileInput);
+    selectedImage.src = fileURL;
+    selectedImage.classList.remove('hidden');
+
     fetch('http://127.0.0.1:5000/predict', {
         method: 'POST',
         body: formData
     })
     .then(response => response.json())
     .then(data => {
-
-        const resultDiv = document.getElementById('result');
-        resultDiv.classList.remove('hidden');
         
         document.querySelectorAll('.progress-bar').forEach(bar => {
-            bar.style.width = '30%'; // Set the width to the final value
+            bar.style.width = '85%'; // Set the width to the final value
         });
 
         // Update progress bar widths based on confidence scores
@@ -37,3 +43,58 @@ document.querySelector('form').addEventListener('submit', function (e) {
         console.error('Error:', error);
     });
 });
+
+const imageInput = document.getElementById('imageInput');
+const selectedImage = document.getElementById('selectedImage');
+const contentContainer = document.querySelector('.content-container');
+
+imageInput.addEventListener('change', function (e) {
+    if (imageInput.files && imageInput.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            selectedImage.src = e.target.result;
+            selectedImage.classList.remove('hidden');
+
+            // Create an image element to get the naturalWidth of the selected image
+            const img = new Image();
+            img.src = selectedImage.src;
+
+            // Wait for the image to load to get its natural width
+            img.onload = function () {
+                const imgWidth = img.naturalWidth;
+                if (imgWidth <= 500) {
+                    contentContainer.style.width = '500px';
+                } else {
+                    // Add 40px to the image width and set it as the container width
+                    contentContainer.style.width = imgWidth + 40 + 'px';
+                }
+            };
+        };
+        reader.readAsDataURL(imageInput.files[0]);
+    }
+});
+
+// JavaScript code to cycle through footer messages
+
+const messages = [
+    "Made with ❤️ in India 🇮🇳",
+    "Made with ❤️ for the world 🌍",
+    "Made with ❤️ by PDEU Students",
+    "Made with ❤️ under the guidance of Prof. Vipul Mishra",
+    "Made with ❤️ by Darshan Rajani",
+    "Made with ❤️ by Swayam Desai",
+    "Made with ❤️ by Heet Savaliya",
+    "Made with ❤️ by Dishita Chudasama",
+    "Made with ❤️ by Prince Koladiya",
+    "Made with ❤️ by Tirth Shah"
+];
+
+const footerMessages = document.getElementById('footer-messages');
+let currentIndex = 0;
+
+function updateFooterMessage() {
+    footerMessages.textContent = messages[currentIndex];
+    currentIndex = (currentIndex + 1) % messages.length;
+}
+
+setInterval(updateFooterMessage, 2000);
